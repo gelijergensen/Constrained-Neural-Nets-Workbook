0892b5a5d8969d8252274eaac2b7bbe48ee5cd2a
G. Eli Jergensen <GEJergensen@lbl.gov>
2019-07-16 16:20:53 -0700

    Monitors are now bare-bones (#29)
    * Monitors are now bare-bones
    
    * Added documentation and a guard for empty values

--- pyinsulate/handlers/monitor.py
+++ pyinsulate/handlers/monitor.py
@@ -1,17 +1,22 @@
-"""A handler which grabs various keys directly from an engine when called"""
+"""A handler which stores objects from an engine which are to be watched"""
 
 
 class Monitor(object):
+    """Monitors are run once at the end of an epoch and can be used to retrieve
+    objects from the engine. Here we simply provide the tools for setting a new
+    value onto the Monitor and for the retrieval of those values with the set()
+    and get() methods."""
 
-    def __init__(self, *keys):
-        """Initializes an empty list for each key to watch"""
-        self._keys = keys
-        for key in self._keys:
-            setattr(self, key, list())
+    def __init__(self):
+        pass  # Nothing needs to happen here for the base class
 
     def __call__(self, engine):
-        for key in self._keys:
-            getattr(self, key).append(getattr(engine, key))
+        raise NotImplementedError
+
+    def set(self, key, value):
+        if not hasattr(self, key):
+            setattr(self, key, list())
+        getattr(self, key).append(value)
 
     def get(self, key):
         return getattr(self, key)
